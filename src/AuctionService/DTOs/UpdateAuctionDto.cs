d1094a3a3f17b4fba67fb2bf817e024550a7e740
shehab43 <Shehabmohamed6n789@gmail.com>
2024-12-07 04:24:22 +0200

    Add SearchService and add MongoDb

--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -6,10 +6,10 @@
         public string Make { get; set; }
 
         public string Model { get; set; }
-        public int Year { get; set; }
+        public int? Year { get; set; }
 
         public string Color { get; set; }
 
-        public int Mileage { get; set; }
+        public int? Mileage { get; set; }
     }
 }

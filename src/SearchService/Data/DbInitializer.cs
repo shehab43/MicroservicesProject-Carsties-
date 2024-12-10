cbe251348352fbf97aed401701783289644fecdf
shehab43 <Shehabmohamed6n789@gmail.com>
2024-12-10 17:37:48 +0200

    adding http Communcation to get the data and Updated Docker-managed named volumes

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Model;
+using SearchService.Service;
 using System.Text.Json;
 
 namespace SearchService.Data
@@ -18,19 +19,29 @@ namespace SearchService.Data
                 .Key(x => x.Color, KeyType.Text)
                 .CreateAsync();
 
-            var count = await DB.CountAsync<Item>();
 
-            if (count == 0)
-            {
-                Console.WriteLine("Not Data -will attempt to seed");
+            using var Scope = app.Services.CreateScope();
 
-                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            var httpClient = Scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClien>();
 
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var items = await httpClient.GetItemsForSearchDb();
 
-                var items =  JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            Console.WriteLine(items.Count + "Returned from the auction service");
 
-              await  DB.SaveAsync(items);
+            if (items.Count > 0) await DB.SaveAsync(items);
+            //var count = await DB.CountAsync<Item>();
+
+            //if (count == 0)
+            //{
+            //    Console.WriteLine("Not Data -will attempt to seed");
+
+            //    var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+            //    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            //    var items =  JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            //  await  DB.SaveAsync(items);
             }
 
         }
--- src/SearchService/RequestHelpers/SearchParams.cs
+++ src/SearchService/RequestHelpers/SearchParams.cs
@@ -2,7 +2,7 @@
 {
     public class SearchParams
     {
-        public string SearchTerm { get; set; }
+        public string? SearchTerm { get; set; }
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 4;
         public string Seller { get; set; }

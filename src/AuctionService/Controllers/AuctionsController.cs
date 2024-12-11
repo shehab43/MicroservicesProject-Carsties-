e3ca6ba368cd7ed4f169814be9dcf68444be94a2
shehab43 <Shehabmohamed6n789@gmail.com>
2024-12-11 19:08:51 +0200

    making our http coummunacation resilient

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -27,7 +27,7 @@ namespace AuctionService.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string? date)
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
         {
             var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
             if (!string.IsNullOrEmpty(date))
